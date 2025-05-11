4cd3c4b0f3545e258096f0aaba59881ce288d065
viralacharya <acharyaviral03@gmail.com>
2025-05-11 21:40:40 +0530

    some fix

--- -Searching_Algo/string_search.js
+++ -Searching_Algo/string_search.js
@@ -3,15 +3,14 @@ function stringSearch(long, pattern) {
 	for (let i = 0; i <= long.length - pattern.length; i++) {
 		for (let j = 0; j < pattern.length; j++) {
 			if (long[i + j] !== pattern[j]) {
-				console.log("Break");
 				break;
 			}
 			if (j === pattern.length - 1) {
-				console.log("Found One");
 				return i;
 			}
 		}
 	}
+	return -1;
 }
 
 //stringSearch("abab cabc abababd", "ababd") // 10
