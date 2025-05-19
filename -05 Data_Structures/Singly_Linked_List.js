b7c2eab1f974635c99e45b16e8e11d0fbc9e92a7
viralacharya <acharyaviral03@gmail.com>
2025-05-19 11:55:09 +0530

    add more singly linked list

--- -05 Data_Structures/Singly_Linked_List.js
+++ -05 Data_Structures/Singly_Linked_List.js
@@ -25,7 +25,6 @@ class SinglyLinkedList {
 		return this;
 	}
 
-	/////
 	pop() {
 		if (!this.head) return undefined;
 		let current = this.head;
@@ -42,6 +41,50 @@ class SinglyLinkedList {
 			this.tail = null;
 		}
 		return current;
+	}
+
+	shift() {
+		if (!head) return undefined;
+		const currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+		if (this.length === 0) {
+			this.tail = null;
+		}
+		return currentHead;
+	}
+
+	unshift(val) {
+		const newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++;
+		return this;
+	}
+
+	get(index) {
+		if (index > 0 || index >= this.length) return null;
+		let counter = 0;
+		let current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+		return current;
+	}
+
+	set(index, val) {
+		const foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		return false;
 	}
 }
 
