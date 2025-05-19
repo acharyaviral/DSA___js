418524121141825a0599d89b4a6635cdfd64beef
viralacharya <acharyaviral03@gmail.com>
2025-05-19 12:45:46 +0530

    singly linked list

--- -05 Data_Structures/Singly_Linked_List.js
+++ -05 Data_Structures/Singly_Linked_List.js
@@ -1,4 +1,3 @@
-//
 class Node {
 	constructor(val) {
 		this.val = val;
@@ -12,6 +11,7 @@ class SinglyLinkedList {
 		this.head = null;
 		this.tail = null;
 	}
+
 	push(val) {
 		const newNode = new Node(val);
 		if (!this.head) {
@@ -44,7 +44,7 @@ class SinglyLinkedList {
 	}
 
 	shift() {
-		if (!head) return undefined;
+		if (!this.head) return undefined;
 		const currentHead = this.head;
 		this.head = currentHead.next;
 		this.length--;
@@ -68,7 +68,7 @@ class SinglyLinkedList {
 	}
 
 	get(index) {
-		if (index > 0 || index >= this.length) return null;
+		if (index < 0 || index >= this.length) return null; // Fixed: was "index > 0"
 		let counter = 0;
 		let current = this.head;
 		while (counter !== index) {
@@ -86,7 +86,60 @@ class SinglyLinkedList {
 		}
 		return false;
 	}
+
+	insert(index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val);
+		if (index === 0) return !!this.unshift(val);
+		const newNode = new Node(val);
+		const prev = this.get(index - 1);
+		const temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+		return true;
+	}
+
+	remove(index) {
+		if (index < 0 || index >= this.length) return undefined;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		const prevNode = this.get(index - 1);
+		const removed = prevNode.next;
+		prevNode.next = removed.next;
+		this.length--;
+		return removed;
+	}
+
+	reverse() {
+		let node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		let next;
+		let prev = null;
+		for (let i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		return this;
+	}
+
+	print() {
+		let current = this.head;
+		let result = "";
+		while (current) {
+			result += `${current.val} -> `;
+			current = current.next;
+		}
+		console.log(result);
+	}
 }
 
+// Example usage
 const list = new SinglyLinkedList();
 list.push("Hello");
+list.push("World");
+list.push("!");
+list.print(); //  display: Hello -> World -> ! ->
