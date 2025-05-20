07e6e005bc499c4b88145def3ac531061dc3ecbf
viralacharya <acharyaviral03@gmail.com>
2025-05-20 19:31:47 +0530

    doubly linked list

--- -05 Data_Structures/Doubly_Linked_List.js
+++ -05 Data_Structures/Doubly_Linked_List.js
@@ -122,6 +122,25 @@ class DoublyLinkedList {
 		this.length++;
 		return true;
 	}
+
+	remove(index) {
+		if (index < 0 || index >= this.length) return undefined;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+
+		const removedNode = this.get(index);
+		const beforeNode = removedNode.prev;
+		const afterNode = removedNode.next;
+
+		beforeNode.next = afterNode;
+		afterNode.prev = beforeNode;
+
+		removedNode.next = null;
+		removedNode.prev = null;
+
+		this.length--;
+		return removedNode;
+	}
 }
 const list = new DoublyLinkedList();
 list.push(1);
