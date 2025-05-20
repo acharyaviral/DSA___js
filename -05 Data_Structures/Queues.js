f07a8230a8389162cf17bf4e2c155907bc403131
viralacharya <acharyaviral03@gmail.com>
2025-05-20 20:18:38 +0530

    add Queues & Stack

---
+++ -05 Data_Structures/Queues.js
@@ -0,0 +1,47 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+	// Adds a value to the end of the queue
+	enqueue(value) {
+		const newNode = new Node(value);
+		if (!this.first) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+		return ++this.size;
+	}
+
+	// Removes a value from the front of the queue
+	dequeue() {
+		if (!this.first) return null;
+		const temp = this.first;
+		if (this.first === this.last) {
+			this.last = null;
+		}
+		this.first = this.first.next;
+		this.size--;
+		return temp.value;
+	}
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.dequeue()); // Output: 2
+console.log(queue.dequeue()); // Output: 3
+console.log(queue.dequeue()); // Output: null
