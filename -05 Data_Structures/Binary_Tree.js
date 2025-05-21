da1639bf9e0a0606aca52de9b1436cd7b8ac8a67
viralacharya <acharyaviral03@gmail.com>
2025-05-21 19:31:43 +0530

    add find in BTS

--- -05 Data_Structures/Binary_Tree.js
+++ -05 Data_Structures/Binary_Tree.js
@@ -35,4 +35,46 @@ class BinarySearchTree {
 			}
 		}
 	}
+
+	// find(value) {
+	// 	if (this.root === null) return false;
+	// 	if (this.root.value === value) return true;
+	// 	let current = this.root;
+	// 	let found = false;
+	// 	while (current && !found) {
+	// 		if (value < current.value) {
+	// 			current = current.left;
+	// 		} else if (value > current.value) {
+	// 			current = current.right;
+	// 		} else {
+	// 			found = true;
+	// 		}
+	// 	}
+	// 	return found;
+	// }
+
+	find(value) {
+		let current = this.root;
+
+		while (current) {
+			if (value === current.value) return true;
+			current = value < current.value ? current.left : current.right;
+		}
+
+		return false;
+	}
 }
+
+const tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+tree.insert(7);
+tree.insert(13);
+tree.insert(17);
+
+console.log(tree.find(7)); // true
+console.log(tree.find(13)); // true
+console.log(tree.find(20)); // false
