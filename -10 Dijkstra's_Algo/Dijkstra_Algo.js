d887c3e5601f3a0478cfdbf3c515d74284f95d4d
viralacharya <acharyaviral03@gmail.com>
2025-05-26 12:16:40 +0530

    update PriorityQueue

--- -10 Dijkstra's_Algo/Dijkstra_Algo.js
+++ -10 Dijkstra's_Algo/Dijkstra_Algo.js
@@ -3,14 +3,69 @@ class PriorityQueue {
 		this.values = [];
 	}
 	enqueue(val, priority) {
-		this.values.push({ val, priority });
-		this.sort();
+		const newNode = new Node(val, priority);
+		this.values.push(newNode);
+		this.bubbleUp();
+	}
+	bubbleUp() {
+		let idx = this.values.length - 1;
+		const element = this.values[idx];
+		while (idx > 0) {
+			const parentIdx = Math.floor((idx - 1) / 2);
+			const parent = this.values[parentIdx];
+			if (element.priority >= parent.priority) break;
+			this.values[parentIdx] = element;
+			this.values[idx] = parent;
+			idx = parentIdx;
+		}
 	}
 	dequeue() {
-		return this.values.shift();
+		const min = this.values[0];
+		const end = this.values.pop();
+		if (this.values.length > 0) {
+			this.values[0] = end;
+			this.sinkDown();
+		}
+		return min;
 	}
-	sort() {
-		this.values.sort((a, b) => a.priority - b.priority);
+	sinkDown() {
+		let idx = 0;
+		const length = this.values.length;
+		const element = this.values[0];
+		while (true) {
+			const leftChildIdx = 2 * idx + 1;
+			const rightChildIdx = 2 * idx + 2;
+			let leftChild;
+			let rightChild;
+			let swap = null;
+
+			if (leftChildIdx < length) {
+				leftChild = this.values[leftChildIdx];
+				if (leftChild.priority < element.priority) {
+					swap = leftChildIdx;
+				}
+			}
+			if (rightChildIdx < length) {
+				rightChild = this.values[rightChildIdx];
+				if (
+					(swap === null && rightChild.priority < element.priority) ||
+					(swap !== null && rightChild.priority < leftChild.priority)
+				) {
+					swap = rightChildIdx;
+				}
+			}
+			if (swap === null) break;
+			this.values[idx] = this.values[swap];
+			this.values[swap] = element;
+			idx = swap;
+		}
+	}
+}
+
+class Node {
+	constructor(val, priority) {
+		this.val = val;
+		this.priority = priority;
 	}
 }
 
@@ -78,7 +133,6 @@ class WeightedGraph {
 	}
 }
 
-// ✅ Sample usage
 const graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
@@ -90,10 +144,11 @@ graph.addVertex("F");
 graph.addEdge("A", "B", 4);
 graph.addEdge("A", "C", 2);
 graph.addEdge("B", "E", 3);
-graph.addEdge("C", "B", 1);
 graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
 graph.addEdge("D", "E", 3);
 graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
 
-console.log(graph.dijkstra("A", "E")); // 👉 Output: [ 'A', 'C', 'B', 'E' ]
+graph.dijkstra("A", "E");
+// Output: [ 'A', 'C', 'D', 'F', 'E' ]
